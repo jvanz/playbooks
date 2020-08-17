6db3f0aa47b87d28375db5e735197c61afac98ab
José Guilherme Vanz <jvanz@jvanz.com>
2020-08-17 14:14:00 -0300

    Removes workstation directory
    Removes the workstation directory because all workstation configuration
    can be some with the Makefile available in the repo with the config
    files.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- workstation/download_dotfile_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Download the archive of the github.com/jvanz/dotfiles to the master machine.
-# Hence, it is not necessary to download the repo in the server used as a
-# workstation (without admin access)
-
-mkdir -p files
-
-wget -O files/dotfiles.zip https://github.com/jvanz/dotfiles/archive/master.zip
